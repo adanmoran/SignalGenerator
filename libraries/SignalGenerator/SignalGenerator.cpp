72ef275813be80221dea35c51b6aca33a84a7401
Adan Moran-MacDonald <adanmoranm@gmail.com>
2019-03-29 08:51:09 -0400

    Fixed some implementation o SignalGenerator, and started on the RealSignalGenerator setup code.

--- libraries/SignalGenerator/SignalGenerator.cpp
+++ libraries/SignalGenerator/SignalGenerator.cpp
@@ -2,17 +2,27 @@
 // SignalGenerator.cpp //
 /////////////////////////
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//               SIGNAL_GENERATOR IMPLEMENTATION
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 #include "SignalGenerator.h"
 
 //--------------//
 // Constructors //
 //--------------//
 
-SignalGenerator::SignalGenerator(const Timer& timer)
-: _type(type),
+SignalGenerator::SignalGenerator(const ArduinoSize& arduino, const Timer& timer)
+: _arduino(arduino),
   _timer(timer)
 {
 	Serial.println("Signal Generator here!");
+
+   if (arduino == ArduinoSize::UNO && (timer == Timer::T3 || timer == Timer::T4))
+   {
+      Serial.println("ArduinoSize/Timer combo is invalid; Uno can only use Timers 0,1,2.");
+      _valid = false;
+   }
+   _valid = true;
 }
 
 SignalGenerator::~SignalGenerator()
@@ -27,6 +37,17 @@ auto SignalGenerator::print() -> void
 	Serial.println("---------------------");
 	Serial.println("Signal Generator v1.0");
 	Serial.println("---------------------");
+
+   Serial.print("Arduino Type specified: ");
+   switch(_arduino)
+   {
+      case ArduinoSize::UNO:
+         Serial.println("Uno");
+         break;
+      case ArduinoSize::DUE:
+         Serial.println("Due");
+         break;
+   }
 
 	Serial.print("Timer Used: ");
 	switch(_timer)
@@ -43,30 +64,88 @@ auto SignalGenerator::print() -> void
 	}
 }
 
+auto SignalGenerator::maxClockSpeed() -> Frequency
+{
+   switch(_arduino)
+   {
+      case ArduinoSize::Uno:
+         return Frequency(UNO_CLOCK_SPEED);
+      case ArduinoSize::Due:
+         return Frequency(DUE_CLOCK_SPEED);
+   }
+   // This will never get here, but let's assume we're on an Uno
+   return Frequency(UNO_CLOCK_SPEED);
+}
+
 //-------------------//
 // Private Functions //
 //-------------------//
 
-/*
-auto SignalGenerator::validate(DigitalPin real, DigitalPin imaginary) -> bool
-{
-	// Check validity of pins
-	if(real > NUM_DIGITAL_PINS)
-	{
-		Serial.println("SignalGenerator: INVALID REAL PIN");
-		return false;
-	}
-	if(_type == SignalType::COMPLEX && imaginary < 0 || imaginary > NUM_DIGITAL_PINS)
-	{
-		Serial.println("SignalGenerator: INVALID IMAGINARY PIN");
-		return false;
-	}
-	
-	// Assign values to save the pins
-	_real = real;
-	_imaginary = imaginary;
-	
-	return true;
-}
-*/
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//             REAL_SIGNAL_GENERATOR IMPLEMENTATION
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
+//----------------//
+// Public Methods //
+//----------------//
+
+auto RealSignalGenerator::setup(Frequency frequency) -> bool
+{
+   // TODO: Set up the arduino to run on PIN_A for this timer
+   // e.g. setup(Frequency, PIN_A) where PIN_A is for this timer
+   return setup(frequency, TIMER0_PWM_PIN_A);
+}
+
+auto RealSignalGenerator::setup(Frequency frequency, DigitalPin timerPin) -> bool
+{
+   // If this is an invalid generator, do nothing
+   if (!isValid())
+   {
+      return false;
+   }
+
+   // If this pin does not match the chosen timer, do nothing
+   if (!pinMatchesTimer(timerPin))
+   {
+      Serial.println("RealSignalGenerator::setup(): pin given is not controlled by the chosen timer.");
+      return false;
+   }
+
+   // TODO: Initialize a hardware-only timer using PWM signals.
+   // TODO: Verify that the given pin matches the current timer, or return false.
+   return false;
+}
+
+//-----------------//
+// Private Methods //
+//-----------------//
+
+auto RealSignalGenerator::pinMatchesTimer(DigitalPin timerPin) -> bool
+{
+   switch(timer())
+   {
+      case Timer::T0:
+         if (timerPin == TIMER0_PWM_PIN_A || timerPin == TIMER0_PWM_PIN_B)
+         { return true; }
+         break;
+      case Timer::T1:
+         if (timerPin == TIMER1_PWM_PIN_A || timerPin == TIMER1_PWM_PIN_B)
+         { return true; }
+         break;
+      case Timer::T2:
+         if (timerPin == TIMER2_PWM_PIN_A || timerPin == TIMER2_PWM_PIN_B)
+         { return true; }
+         break;
+      case Timer::T3:
+         if (timerPin == TIMER3_PWM_PIN_A || timerPin == TIMER3_PWM_PIN_B || timerPin == TIMER3_PWM_PIN_C)
+         { return true; }
+         break;
+      case Timer::T4:
+         if (timerPin == TIMER4_PWM_PIN_A || timerPin == TIMER4_PWM_PIN_B || timerPin == TIMER4_PWM_PIN_C)
+         { return true; }
+         break;
+   }
+   return false;
+}
+
+// vim: ts=3 sw=3 sts=0 et: 
--- libraries/SignalGenerator/SignalGenerator.h
+++ libraries/SignalGenerator/SignalGenerator.h
@@ -32,8 +32,19 @@ Last Modified 22 Jan 2018
 #define TIMER4_PWM_PIN_B 7
 #define TIMER4_PWM_PIN_C 8
 
+/**
+ * Define clock speed constants for handling different
+ * Arduinos. Like was mentioned above, we do this instead of static consts
+ * to save memory space.
+ */
+#define UNO_CLOCK_SPEED = 16000000 //16MHz
+#define DUE_CLOCK_SPEED = 84000000 //84MHz
+
 // Define a pin type for digital signals, which must be positive and are not big numbers.
 using DigitalPin = int8_t;
+
+// Define a type for frequency values in Hz, which must be positive.
+using Frequency = uint64_t;
 
 /*
 Enumeration object to define which timers to use for signal generation
@@ -66,9 +77,12 @@ enum class ArduinoSize
 class SignalGenerator
 {
 	public:
-		
-		
-	SignalGenerator(const Timer& timer);
+   /**
+    * Initialize a SignalGenerator for this arduino type, on this timer.
+    * Note that the UNO only has access to Timer0, Timer1, Timer2 and 
+    * so the combination will only be valid for Uno if it is one of those timers.
+    */
+	SignalGenerator(const ArduinoSize& arduino, const Timer& timer);
 	~SignalGenerator();
 	
 	/**
@@ -77,26 +91,90 @@ class SignalGenerator
 	 * - timer choice (T1, T2, or T3)
 	 */
 	auto print() -> void;
-	
+
+	/**
+    * Get the clock speed for this arduino, in Hz
+    */
+   auto maxClockSpeed() -> Frequency;
 	/**
 	 * Read which timer the signal generator is using`
 	 */
+
 	auto timer() -> const Timer& 
 	{ return timer_; }
+
+   /**
+    * Tell which type of Arduino this is.
+    */
+   auto type() -> const ArduinoSize&
+   { return _arduino; }
+
+   /**
+    * Returns true if the ArduinoSize/Timer combination inputted is valid.
+    */
+   auto isValid() -> bool
+   { return _valid; }
+
+   /**
+    * Set up the timer to run at a given frequency on PIN_A
+    */
+   virtual auto setup(Frequency) -> bool = 0;
+
+   /**
+    * Run the signal generator
+    */
+   virtual auto run() -> bool = 0;
+
+   /**
+    * Stop the signal generator
+    */
+   virtual auto stop() -> bool = 0;
 	
 	private:
 	Timer _timer;
+   ArduinoSize& _arduino;
+   bool _valid = false;
 };
 
 /* RealSignalGenerator
  * Uses the timer from the SignalGenerator class and generates
  * a real-valued signal.
  */
- 
+class RealSignalGenerator : public SignalGenerator
+{
+   public:
+   /**
+    * Initialize a Signalgenerator to start at the gien timer.
+    */
+   RealSignalGenerator(const ArduinoSize& arduino, const Timer& timer)
+      : SignalGenerator(arduino, timer) {}
+
+   /** 
+    * Set up the signal to run on PIN_A at the inputted frequency.
+    */
+   auto setup(Frequency frequency) -> bool;
+
+   /**
+    * Set up the signal to run on the given pin. This must match one of the
+    * pins available to the timer.
+    */
+   auto setup(Frequency frequency, DigitalPin timerPin) -> bool;
+
+   private:
+
+   /** 
+    * Verify that the given digital pin matches the pins for this timer
+    */
+   auto pinMatchesTimer(DigitalPin) -> bool;
+
+   
+};
+
  // TODO: the signal should always be on a hardwware pin (no ISR routines here). How do we enforce that? How do users choose whether it's on PIN_A,B, or C??? 
  // TODO: the signal should have a default frequency, plus they should be allowed to modify the frequency at runtime. How to do this?
  // TODO: make a complex signal generator that uses two real signal generators, and allow the user to choose PIN_A, B, or C as the real and another as complex.
- // TODO: These should all be templates with the ArduinoSize as a template parameter. Should it instead be an input to the constructor?
+ // TODO: Alternatively, make a complex signal generator that toggles the two pins using one timer with hardware, as we did for the heart rate monitor in February.
  
 
 #endif
+// vim: ts=3 sw=3 sts=0 et: 
